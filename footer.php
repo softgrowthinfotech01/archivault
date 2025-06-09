49eae3865c1261b2d5a0e60225b50f4af37d0a1e
shradhasapat <shradhasapat191991@gmail.com>
2025-06-09 12:57:37 +0530

    componets separted

---
+++ footer.php
@@ -0,0 +1,100 @@
+ <!-- Footer Area -->
+    <footer class="footer-area">
+        <div class="container">
+            <div class="footer-up">
+                <div class="row gy-5">
+                    <div class="col-lg-4 col-md-6 col-sm-12">
+                        <a href="index.html" class="logo">
+                            <img src="assets/img/logo-white.png" alt="logo">
+                        </a>
+                        <p>We provide the best window & door <br> services with reliable care.
+                        </p>
+                    </div>
+                    <div class="col-lg-2 col-md-6">
+                        <h5>Office</h5>
+                        <p>Corporate Office- <br>
+                            175 24th Street, OT- 35
+                            London, UK 265
+                        </p>
+                        <div class="company-email">
+                            <a href="#">info@windox.com</a>
+                        </div>
+                        <div class="phone-number">
+                            <p>+18-4675826</p>
+                        </div>
+
+                    </div>
+                    <div class="col-lg-2 offset-lg-1 col-md-6 com-sm-12">
+                        <h5>Links</h5>
+                        <ul>
+                            <li>
+                                <a href="about.html">About</a>
+                                <a href="services.html">Services</a>
+                                <a href="portfolio-grid.html">Portfolio</a>
+                                <a href="price.html">Pricing</a>
+                                <a href="faq.html"> FAQ</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-3 col-md-6 col-sm-12">
+                        <h5>Get in Touch</h5>
+                        <ul>
+                            <li>
+                                <div class="social-area">
+                                    <a href="#"><i class="lab la-facebook-f"></i>Facebook</a>
+                                    <a href="#"><i class="lab la-instagram"></i>Instagram</a>
+                                    <a href="#"><i class="lab la-linkedin-in"></i>linkedin</a>
+                                    <a href="#"><i class="la la-skype"></i>Skype</a>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- Footer Bottom Area -->
+
+    <div class="footer-bottom">
+        <div class="container">
+            <div class="row justify-content-center align-items-center justify-content-center">
+                <div class="col-lg-6 col-md-6 col-sm-12">
+                    <p class="copyright-line">© 2025 Windox. All rights reserved.</p>
+                </div>
+                <div class="col-lg-6 col-md-6 col-xs-12 text-md-end">
+                    <p class="privacy">Privacy Policy | Terms &amp; Conditions</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Search Dropdown -->
+
+    <div class="search-popup">
+        <span class="search-back-drop"></span>
+
+        <div class="search-inner">
+            <div class="container">
+                <div class="logo">
+                    <a class="navbar-brand" href="index.html"><img src="assets/img/logo-white.png" alt=""></a>
+                </div>
+                <button class="close-search"><span class="la la-times"></span></button>
+                <form method="post" action="index.html">
+                    <div class="form-group">
+                        <input type="search" name="search-field" value="" placeholder="Type your keyword" required="">
+                        <button type="submit"><i class="fal fa-search"></i></button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- back to top start -->
+    <div class="progress-wrap">
+        <svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
+            <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
+        </svg>
+    </div>
---
+++ header.php
@@ -0,0 +1,196 @@
+<div id="loader"></div>
+
+    <!-- Mouse Cursor  -->
+    <div class="mouseCursor cursor-outer"></div>
+    <div class="mouseCursor cursor-inner"><span>Drag</span></div>
+
+    <!-- Header Top Area -->
+
+    <div class="header-top-area dark-bg">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-8 col-lg-8">
+                    <span><i class="fa-light fa-clock"></i>Sunday - Friday: 9 am - 8 pm</span>
+                    <span><i class="fa-light fa-envelope"></i>info@example.com</span>
+                    <span><i class="fa-light fa-location-dot"></i>6391 Elgin St. Celina, 10299</span>
+                </div>
+                <div class="col-xl-4 col-lg-4 text-end">
+                    <div class="social-area">
+                        <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                        <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                        <a href="#"><i class="fa-brands fa-youtube"></i></a>
+                        <a href="#"><i class="fa-brands fa-linkedin"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Header Area  -->
+    <div class="header-area absolute-header">
+        <div id="header-sticky">
+            <div class="navigation">
+                <div class="container-fluid">
+                    <div class="header-inner-box">
+
+                        <!-- Logo -->
+                        <div class="logo">
+                            <a class="navbar-brand" href="index.html"><img src="assets/img/logo-white.png" alt=""></a>
+                        </div>
+
+                        <!-- Main Menu  -->
+                        <div class="main-menu d-none d-lg-block">
+                            <ul>
+                                <li class="active"><a class="navlink" href="#">Home</a>
+                                    <ul class="sub-menu">
+                                        <!-- <li><a class="active" href="index.html">Home - Main</a></li>
+                                        <li><a href="index-2.html">Home - Doors </a></li>
+                                        <li><a href="index-3.html">Home - Windows</a></li>
+                                        <li><a href="index-4.html">Home - Furniture</a></li> -->
+                                        <li><a href="index-5.html">Home - Interior</a></li>
+                                    </ul>
+                                </li>
+                                <li><a class="navlink" href="#">About us</a>
+                                    <ul class="sub-menu">
+                                        <li><a href="about">About Us</a></li>
+                                        <li><a href="services">Our Services</a></li>                                        
+                                        <!-- <li><a href="team.html">Our Team</a></li>
+                                        <li><a href="price.html">Pricing</a></li>
+                                        <li><a href="faq.html">FAQs</a></li> -->
+                                        <li><a href="quote.html">Request a Quote</a></li>
+                                    </ul>
+                                </li> 
+                                <li><a class="navlink" href="#">Services</a>
+                                    <ul class="sub-menu">
+                                        <li><a href="services.html">Services</a></li>
+                                        <li><a href="service-details.html">Service Details</a></li>
+                                    </ul>
+                                </li>                               
+                                <li><a class="navlink" href="#">Our Work</a>
+                                    <ul class="sub-menu">
+                                        <li><a href="portfolio-standard.html">Standard</a></li>
+                                        <!-- <li><a href="portfolio-metro.html">Metro Fullwidth</a></li>                                        
+                                        <li><a href="portfolio-grid.html">Grid</a></li>
+                                        <li><a href="portfolio-list.html">List</a></li>
+                                        <li><a href="portfolio-masonry.html">Masonry</a></li>
+                                        <li><a href="portfolio-slider.html">Slider</a></li>                                        
+                                        <li class="nav-item">
+                                            <a href="#">Single Portfolio <i class="las la-arrow-right"></i>
+                                                <span class="sub-nav-toggler"></span>
+                                            </a>
+                                            <ul class="sub-menu-two">
+                                                <li><a href="portfolio-details-left-sidebar.html">Left Sidebar</a></li>
+                                                <li><a href="portfolio-details-right-sidebar.html">Right Sidebar</a></li>
+                                                <li><a href="portfolio-details-gallery.html">Gallery</a></li>
+                                            </ul>
+                                        </li> -->
+                                    </ul>
+                                </li>
+                                <!-- <li><a class="navlink" href="#">Blog</a>
+                                    <ul class="sub-menu">
+                                        <li><a href="blog-standard.html">Blog- Standard</a></li>
+                                        <li><a href="blog-grid.html">Blog- Grid</a></li>
+                                        <li><a href="blog-details.html">Blog Details</a></li>
+                                    </ul>
+                                </li> -->
+
+                                <li><a class="navlink" href="contact.html">Contact</a></li>
+                            </ul>
+                        </div>
+
+                        <div class="header-right">
+
+                            <!-- Search Button  -->
+                            <div class="search-trigger">
+                                <i class="fal fa-search"></i>
+                            </div>
+                            <div class="contact-number d-none">
+                                <div class="icon"><i class="las la-phone-volume"></i></div>
+                                <div class="title"><h4>1 800 458 56 97</h4></div>
+                            </div>
+                            <!-- Header Button -->
+                            <a href="quote.html" class="theme-btn d-none d-lg-inline-block">Request a Quote</a>
+
+                        </div>
+                        <!-- Mobile Menu -->
+                        <div class="mobile-nav-bar d-block col-sm-1 col-6 d-lg-none">
+                            <div class="mobile-nav-wrap">
+                                <div id="hamburger">
+                                    <i class="las la-bars"></i>
+                                </div>
+                                <!-- mobile menu - responsive menu  -->
+                                <div class="mobile-nav">
+                                    <button type="button" class="close-nav">
+                                        <i class="las la-times-circle"></i>
+                                    </button>
+                                    <nav class="sidebar-nav">
+                                        <ul class="metismenu" id="mobile-menu">
+                                            <li><a class="has-arrow" href="home">Homes</a>
+                                                <ul class="sub-menu">
+                                                    <!-- <li><a class="active" href="index.html">Home - Main</a></li>
+                                                    <li><a href="index-2.html">Home - Doors </a></li>
+                                                    <li><a href="index-3.html">Home - Windows</a></li>
+                                                    <li><a href="index-4.html">Home - Furniture</a></li> -->
+                                                    <li><a href="index-5.html">Home - Interior</a></li>
+                                                </ul>
+                                            </li>
+                                            <li><a class="has-arrow" href="#">About us</a>
+                                                <ul class="sub-menu">
+                                                    <li><a href="about">About Us</a></li>
+                                                    <li><a href="services">Our Services</a></li>                                        
+                                                    <!-- <li><a href="team.html">Our Team</a></li>
+                                                    <li><a href="price.html">Pricing</a></li>
+                                                    <li><a href="faq.html">FAQs</a></li> -->
+                                                    <li><a href="quote.html">Request a Quote</a></li>
+                                                </ul>
+                                            </li>
+                                            <li><a class="has-arrow" href="#">Services</a>
+                                                <ul class="sub-menu">
+                                                    <li><a href="services.html">Services</a></li>
+                                                    <li><a href="service-details.html">Service Details</a></li>
+                                                </ul>
+                                            </li>
+                                            <li><a class="has-arrow" href="#">Our Work</a>
+                                                <ul class="sub-menu">
+                                                    <li><a href="portfolio-standard.html">Standard</a></li>
+                                                    <!-- <li><a href="portfolio-metro.html">Metro Fullwidth</a></li>                                        
+                                                    <li><a href="portfolio-grid.html">Grid</a></li>
+                                                    <li><a href="portfolio-list.html">List</a></li>
+                                                    <li><a href="portfolio-masonry.html">Masonry</a></li>
+                                                    <li><a href="portfolio-slider.html">Slider</a></li> -->
+                                                    <!-- <li class="nav-item">
+                                                        <a href="#">Single Portfolio <i class="las la-arrow-right"></i>
+                                                            <span class="sub-nav-toggler"></span>
+                                                        </a>
+                                                        <ul class="sub-menu-two">
+                                                            <li><a href="portfolio-details-left-sidebar.html">Left Sidebar</a></li>
+                                                            <li><a href="portfolio-details-right-sidebar.html">Right Sidebar</a></li>
+                                                            <li><a href="portfolio-details-gallery.html">Gallery</a></li>
+                                                        </ul>
+                                                    </li> -->
+                                                </ul>
+                                            </li>
+                                            <!-- <li><a class="has-arrow" href="#">Blog</a>
+                                                <ul class="sub-menu">
+                                                    <li><a href="blog-standard.html">Blog - Standard</a></li>
+                                                    <li><a href="blog-grid.html">Blogs - Grid</a></li>
+                                                    <li><a href="blog-details.html">Blog Details</a></li>
+                                                </ul>
+                                            </li> -->
+                                            <li><a href="contact.html">Contact</a></li>
+
+                                        </ul>
+                                    </nav>
+                                    <div class="action-bar">
+                                        <a href="mailto:info@roofix.com"><i class="las la-envelope"></i>info@windox.com</a>
+                                        <a href="tel:123-456-7890"><i class="fal fa-phone"></i>123-456-7890</a>
+                                        <a href="contact.html" class="theme-btn">Contact Us</a>
+                                    </div>
+                                </div>
+                            </div>                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
