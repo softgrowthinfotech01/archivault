ca524ff13480e169e541436fac8990b5c4a3337c
AachalGedam <aachalgedam34@gmail.com>
2025-06-13 13:46:46 +0530

    add

--- home.php
+++ home.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
 
-    <title>He</title>
+    <title>Archivault</title>
 
     <!--Favicon-->
     <link rel="icon" href="assets/img/favicon.png" type="image/png">
